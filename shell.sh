aca00e6e5b9896d6fa3dcc07aae3df26b417f0b3
mrinmoymajumdar17041995 <mrinmoymajumdar564@gmail.com>
2023-05-12 06:15:28 +0000

    Commiting my second file

---
+++ shell.sh
@@ -0,0 +1,10 @@
+ECHO "***********Starting the script**********************"
+mkdir mrinmoy
+cd mrinmoy
+touch file1 > "This is my first file"
+touch file2 > "This is my second file"
+ls
+ECHO "Displaying the files"
+cat file1
+cat file2
+ECHO "**************End of File*****************************"

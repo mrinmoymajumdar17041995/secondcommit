acde9cf4c307a4f5b176fda85255cee9cc073d8f
mrinmoy majumdar <36600397+mrinmoymajumdar17041995@users.noreply.github.com>
2023-05-12 14:54:20 +0530

    Updated shell.sh

--- shell.sh
+++ shell.sh
@@ -1,10 +1,10 @@
-ECHO "***********Starting the script**********************"
-mkdir mrinmoy
-cd mrinmoy
+echo "***********Starting the script**********************"
+mkdir ronnie
+cd ronnie
 touch file1 > "This is my first file"
 touch file2 > "This is my second file"
 ls
 ECHO "Displaying the files"
 cat file1
 cat file2
-ECHO "**************End of File*****************************"
+echo "**************End of File*****************************"
